98ee17fdbbd160952fdd95fc8cc318d15e05df85
suraj192 <surajsharma1921@gmail.com>
2022-02-26 13:30:00 +0200

    Dom practise and task_5

--- margit_lesson/java_script/1_task/main.js
+++ margit_lesson/java_script/1_task/main.js
@@ -3,3 +3,29 @@ alert("Welcome to Java Script");
 //   return "hello";
 // }
 // console.log(hello());
+let ind = document.querySelector("#inputdata");
+console.log(ind);
+let he = document.getElementsByTagName("h1");
+console.log(he);
+let an = document.querySelector("h1");
+console.log(an);
+let the = document.getElementsByTagName("h1")[0];
+console.log(the);
+let evl = document.querySelector("#inputdata");
+const hello = (event) => {
+  event.preventDefault();
+  let price = document.querySelector("#price").value;
+  console.log(price);
+};
+evl.addEventListener("submit", hello);
+console.log(document.querySelector("#mybtn"));
+
+let formData = {};
+formData["licence"] = "sdf-343";
+formData["maker"] = "kia";
+formData["owner"] = "Suraj";
+formData["model"] = "niro";
+formData["price"] = "32000";
+formData["color"] = "white";
+
+console.log(formData.length);
