b19189335a59af8ec781d4481fb111ca6cbffcb7
suraj192 <surajsharma1921@gmail.com>
2022-03-18 15:08:46 +0200

    portfolio design upgrade

--- margit_lesson/html_and_css/portfolio/main.js
+++ margit_lesson/html_and_css/portfolio/main.js
@@ -36,51 +36,43 @@ const mobMenu = () => {
     document.body.style.overflow = "hidden";
   }
 };
+
+const readMore = (dot, moretext, btntext) => {
+  if (dot.style.display === "none") {
+    dot.style.display = "inline";
+    btntext.innerHTML = "Read more";
+    moretext.style.display = "none";
+  } else {
+    dot.style.display = "none";
+    btntext.innerHTML = "Read less";
+    moretext.style.display = "inline";
+  }
+};
+
 function myFunction() {
   let dots = document.getElementById("dots1");
   let moreText = document.getElementById("more1");
   let btnText = document.getElementById("myBtn1");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
+  readMore(dots, moreText, btnText);
 }
 function myFunction1() {
   let dots = document.getElementById("dots2");
   let moreText = document.getElementById("more2");
   let btnText = document.getElementById("myBtn2");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
+  readMore(dots, moreText, btnText);
 }
 function myFunction2() {
   let dots = document.getElementById("dots3");
   let moreText = document.getElementById("more3");
   let btnText = document.getElementById("myBtn3");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
+  readMore(dots, moreText, btnText);
 }
 
 button.addEventListener("click", getToTop);
 mobButton.addEventListener("click", mobMenu);
+
+// console.log(document.getElementById("more2"));
+// console.log(document.querySelector("#more2"));
